3644db69a993a9056c9e016d9a110203d643de74
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-18 15:42:18 -0500

    Refactor ChatWidget.vue and ai.post.ts files

--- components/ChatWidget.vue
+++ components/ChatWidget.vue
@@ -1,27 +1,47 @@
 <script setup lang="ts">
-import type { Message, User } from "@/types";
+import type { Message, User } from '@/types'
 
 const me = ref<User>({
-  id: "user",
-  avatar: "/avatar.jpg",
-  name: "You",
-});
+  id: 'user',
+  avatar: '/avatar.jpg',
+  name: 'You',
+})
 const bot = ref<User>({
-  id: "assistant",
-  avatar: "/bot.jpg",
-  name: "Botman",
-});
+  id: 'assistant',
+  avatar: '/bot.jpg',
+  name: 'Botman',
+})
 
-const users = computed(() => [me.value, bot.value]);
+const users = computed(() => [me.value, bot.value])
 
-const messages = ref<Message[]>([]);
+const messages = ref<Message[]>([])
 
-const usersTyping = ref<User[]>([]);
+const usersTyping = ref<User[]>([])
 
 // send messages to Chat API here
 // and in the empty function below
 
-async function handleNewMessage(message: Message) {}
+async function handleNewMessage(message: Message) {
+  messages.value.push(message)
+  usersTyping.value.push(bot.value)
+  const res = await $fetch('/api/ai', {
+    method: 'POST',
+    body: {
+      messages: [{ role: 'user', content: message.text }],
+    },
+  })
+
+  if (!res.choices[0].message?.content) return
+
+  const msg: Message = {
+    id: res.id,
+    userId: bot.value.id,
+    createdAt: new Date(),
+    text: res.choices[0].message.content,
+  }
+  messages.value.push(msg)
+  usersTyping.value = []
+}
 </script>
 <template>
   <ChatBox
--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -5,6 +5,7 @@ const { openai: openaiConfig } = useRuntimeConfig()
 export default defineEventHandler(async (event) => {
 
   const body = await readBody(event)
+  console.log('body', body)
 
   const openai = new OpenAI({
     apiKey: openaiConfig.OPENAI_API_KEY,
