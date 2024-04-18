a776da4fee90b3b4996f6562b0a7584ce5b85903
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-18 13:52:32 -0500

    Add user message to AI chat endpoint

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -3,13 +3,17 @@ import OpenAI from 'openai'
 const { openai: openaiConfig } = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
+
+  const body = await readBody(event)
+
   const openai = new OpenAI({
     apiKey: openaiConfig.OPENAI_API_KEY,
   })
 
   const completion = await openai.chat.completions.create({
-    messages: [{ role: 'system', content: 'You are a helpful assistant.' }],
     model: 'gpt-3.5-turbo',
+    messages: body.messages || [],
+    temperature: body.temperature || 1,
   })
 
   return completion
