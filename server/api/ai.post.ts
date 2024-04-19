4b7f4a949d33d421b7e1368625390c632e9a2ae3
Elvis Saavedra <elvis.saavedra@nearsure.com>
2024-04-18 21:44:31 -0500

    added agent parameter to send to chatgtp

--- server/api/ai.post.ts
+++ server/api/ai.post.ts
@@ -1,11 +1,16 @@
 import OpenAI from 'openai'
-import { customerSupportAgent } from '~/agents'
+import * as agents from '~/agents'
 
 const { openai: openaiConfig } = useRuntimeConfig()
 
 export default defineEventHandler(async (event) => {
 
   const body = await readBody(event)
+  const agent = body.agent || 'customerSupportAgent'
+
+  if (!Object.keys(agents).includes(agent)) {
+    throw new Error(`Agent ${agent} not found`)
+  }
 
   const openai = new OpenAI({
     apiKey: openaiConfig.OPENAI_API_KEY,
@@ -15,7 +20,8 @@ export default defineEventHandler(async (event) => {
     model: 'gpt-3.5-turbo',
     messages: body.messages || [],
     temperature: body.temperature || 1,
-    ...customerSupportAgent(body),
+    // @ts-expect-error already checked above
+    ...agents[agent](body),
   })
 
   return completion
